b6308b4cb70d2e7c8c859c257a2fd57179e8d85d
Daniel San <106621051+DanielBlem93@users.noreply.github.com>
2024-10-21 14:50:24 +0200

    added singup page

--- src/app/login/footer/footer.component.ts
+++ src/app/login/footer/footer.component.ts
@@ -1,12 +1,18 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
 export class FooterComponent {
+
+  constructor(public router: Router) {
+
+  }
 
 }
--- src/app/login/sing-up/sing-up.component.ts
+++ src/app/login/sing-up/sing-up.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from "../header/header.component";
+import { FooterComponent } from "../footer/footer.component";
 
 @Component({
   selector: 'app-sing-up',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './sing-up.component.html',
   styleUrl: './sing-up.component.scss'
 })
