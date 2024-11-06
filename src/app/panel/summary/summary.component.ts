8451ae26d349fb6fb25d045fc27f28fe5e889c66
Daniel San <106621051+DanielBlem93@users.noreply.github.com>
2024-11-06 18:22:16 +0100

    added mixin to summary css

--- src/app/panel/summary/summary.component.ts
+++ src/app/panel/summary/summary.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { NavbarComponent } from "../navbar/navbar.component";
 
 import { PanelHeaderComponent } from "../panel-header/panel-header.component";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-summary',
   standalone: true,
-  imports: [NavbarComponent,PanelHeaderComponent],
+  imports: [NavbarComponent,PanelHeaderComponent, RouterLink],
   templateUrl: './summary.component.html',
   styleUrl: './summary.component.scss'
 })
