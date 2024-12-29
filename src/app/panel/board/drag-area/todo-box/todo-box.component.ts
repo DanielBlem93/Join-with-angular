25dfd986d6dfffb7b9078b5251026d6b7c662f4d
Daniel San <106621051+DanielBlem93@users.noreply.github.com>
2024-12-29 19:18:16 +0100

    renders asseinged persons on to do box

--- src/app/panel/board/drag-area/todo-box/todo-box.component.ts
+++ src/app/panel/board/drag-area/todo-box/todo-box.component.ts
@@ -1,17 +1,20 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { CompletedSubtasksPipe } from '../../../../pipes/completed-subtasks.pipe';
 import { CommonModule } from '@angular/common';
+import { Task } from '../../../../models/task.class';
+import { Tasks } from '../../../../interfaces/tasks';
+import { GetInitalsPipe } from '../../../../pipes/get-initals.pipe';
 
 
 @Component({
   selector: 'app-todo-box',
   standalone: true,
-  imports: [CompletedSubtasksPipe, CommonModule],
+  imports: [CompletedSubtasksPipe, CommonModule, GetInitalsPipe],
   templateUrl: './todo-box.component.html',
   styleUrl: './todo-box.component.scss'
 })
 export class TodoBoxComponent {
-  @Input() task: any;
+  @Input() task!: Tasks;
   @Input() arrayName: string ="";
 
 
