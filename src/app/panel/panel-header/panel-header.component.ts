eb3812c89a74d2846f06985213a9570208febd60
Daniel San <106621051+DanielBlem93@users.noreply.github.com>
2025-01-03 15:37:11 +0100

    changed logout button

--- src/app/panel/panel-header/panel-header.component.ts
+++ src/app/panel/panel-header/panel-header.component.ts
@@ -1,17 +1,24 @@
 import { Component } from '@angular/core';
 import { AuthenticationService } from '../../services/authentication.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-panel-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './panel-header.component.html',
   styleUrl: './panel-header.component.scss'
 })
 export class PanelHeaderComponent {
 
+
+  logoutMenu = false;
   constructor(public authService: AuthenticationService) {
 
   }
 
+
+  toggleLogoutMenu() {
+    this.logoutMenu = !this.logoutMenu;
+  }
 }
