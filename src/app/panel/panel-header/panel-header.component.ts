0ccebc3f228785aade45a0645b4dded83241da81
Daniel San <106621051+DanielBlem93@users.noreply.github.com>
2025-01-05 14:56:11 +0100

    added last logic ready for responsive

--- src/app/panel/panel-header/panel-header.component.ts
+++ src/app/panel/panel-header/panel-header.component.ts
@@ -1,6 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener } from '@angular/core';
 import { AuthenticationService } from '../../services/authentication.service';
 import { CommonModule } from '@angular/common';
+import { HelpersService } from '../../services/helpers.service';
 
 @Component({
   selector: 'app-panel-header',
@@ -13,12 +14,23 @@ export class PanelHeaderComponent {
 
 
   logoutMenu = false;
-  constructor(public authService: AuthenticationService) {
-
+  constructor(public authService: AuthenticationService,
+    private elRef: ElementRef,
+    public helpers: HelpersService
+  ) {
+    
   }
 
 
   toggleLogoutMenu() {
     this.logoutMenu = !this.logoutMenu;
+  }
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: Event): void {
+    const targetElement = event.target as HTMLElement;
+    if (this.logoutMenu && !this.elRef.nativeElement.contains(targetElement)) {
+      this.logoutMenu = false;
+    }
   }
 }
